d3859940fa4e83f577e8fc2bc48472e4b9dc19f4
Stephen <Ste.a.curran@gmail.com>
2016-11-04 15:34:38 +0000

    Update CA2.java

--- CA2/CA2/src/ca2/CA2.java
+++ CA2/CA2/src/ca2/CA2.java
@@ -2,7 +2,7 @@ package ca2;
 
 /**
  *
- * @author X00136103+ X00137811
+ * @author X00136103 + X00137811
  */
 import java.util.Scanner;
 public class CA2 {
@@ -16,9 +16,10 @@ public class CA2 {
         final int Fee_paid_majority=;
         final int Fee_paid_half=;
         final int Fee_paid_none=;
-        String username1, password1, password2, student_name,student_number;
-        int attempts,selection, SDev_reuslt,Maths_reuslt,L2L_reuslt,CompArch_reuslt,average_reuslt,overall_average_reuslt, 
-        student_amount_paid,number_of_students_proccessed;
+        String Username1, Password1, Student_Name,Student_Number;
+        int Attempts,Selection, SDev_Reuslt,Maths_Reuslt,L2L_Reuslt,
+            CompArch_Reuslt,Average_Reuslt,Overall_Average_Reuslt, 
+            Student_Amount_Paid,Number_Of_Students_Proccessed;
         
             
         System.out.println("Please enter your username");
@@ -38,11 +39,50 @@ public class CA2 {
          System.out.println("Password or Username is incorrect");
          attempts++;
         }
-        if (attempts = 3){
+        if (Attempts = 3){
         System.exit(0);
         }
-        int selection = 0
+        int Selection = 0
             
+             while(Selection != 5){
+ +            System.out.printf("Student Finance System%n"
+ +                    + "1. Calculate Student Grant%n"
+ +                    + "2. Current Overall Student Average%n"
+ +                    + "3. Fee Data%n"
+ +                    + "4. Grant Category Information%n"
+ +                    + "5. Exit");
+ +            System.out.println("Select option");
+ +            Selection = input.nextInt();
+ +            switch(Selection){
+ +                case 1:
+ +                    System.out.println("You have selected "+"Calculate Student Grant");
+ +                    String Student_Name;
+ +                    String Student_Number;
+ +                    System.out.println("Enter your name");
+ +                    input.nextLine();
+ +                    Student_Name = input.nextLine();
+ +                    System.out.println("Enter your student number.");
+ +                    Student_Number = input.nextLine();
+ +                    while(Student_Number.charAt(0) != 'X'){
+ +                        System.out.println("Invalid Student Number format. Number must begin with uppercase 'X'. Please re-enter.");
+ +                        Student_Number = input.nextLine();
+ +                    }
+ +                    break;
+ +                case 2:
+ +                    System.out.println("Option 2");
+ +                    break;
+ +                case 3:
+ +                    System.out.println("Option 3");
+ +                    break;
+ +                case 4:
+ +                    System.out.println("Option 4");
+ +                    break;
+ +                case 5:
+ +                    System.exit(0);
+ +                    break;
+ +                default:
+ +                    System.out.println("Invalid selection. Please re-enter");
+ +                    break;
             
        
         
