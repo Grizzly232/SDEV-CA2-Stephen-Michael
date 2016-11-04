d536ac9ab02757158524d674a8a824c70f233198
Stephen <Ste.a.curran@gmail.com>
2016-11-04 13:55:24 +0000

    update Ca2.java
    Attempt 1 Incomplete

--- CA2/CA2/src/ca2/CA2.java
+++ CA2/CA2/src/ca2/CA2.java
@@ -1,21 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ca2;
 
 /**
  *
- * @author X00136103
+ * @author X00136103+ X00137811
  */
+import java.util.Scanner;
 public class CA2 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner key = new Scanner(System.in);
+        final int Fee_paid_all=;
+        final int Fee_paid_majority=;
+        final int Fee_paid_half=;
+        final int Fee_paid_none=;
+        String username1, password1, password2, student_name,student_number;
+        int attempts,selection, SDev_reuslt,Maths_reuslt,L2L_reuslt,CompArch_reuslt,average_reuslt,overall_average_reuslt, 
+        student_amount_paid,number_of_students_proccessed;
+        
+            
+        System.out.println("Please enter your username");
+        username1= key.nextLine();
+        
+        System.out.println("Please enter your password");
+        password1= key.nextLine();
+        
+        System.out.println("Please confirm your password");
+        attempts=0
+        while ( attempts!= 3)
+         if (input.nextLine().equals(password1)) {
+         System.out.println("Password accepted");
+        
+        }
+         else {
+         System.out.println("Password or Username is incorrect");
+         attempts++;
+        }
+        if (attempts = 3){
+        System.exit(0);
+        }
+        int selection = 0
+            
+            
+       
+        
     }
     
 }
